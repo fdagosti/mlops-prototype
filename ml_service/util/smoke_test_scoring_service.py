859a32f2a19a7af40e1f38c50899aaaeca7a4797
Francois D'Agostini <dagostini.francois@bcg.com>
2022-01-13 18:33:13 -0500

    printing keys

--- ml_service/util/smoke_test_scoring_service.py
+++ ml_service/util/smoke_test_scoring_service.py
@@ -27,6 +27,8 @@ def call_web_service(e, service_type, service_name):
     if service.auth_enabled:
         service_keys = service.get_keys()
         headers['Authorization'] = 'Bearer ' + service_keys[0]
+        print("header")
+        print(headers['Authorization'])
     print("Testing service")
     print(". url: %s" % service.scoring_uri)
     output = call_web_app(service.scoring_uri, headers)
